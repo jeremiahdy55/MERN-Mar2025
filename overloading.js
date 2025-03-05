4d6c9e65c34d7f2f5b01029873cc3af1935315fb
Jeremiah Dy <jeremiahjava55@gmail.com>
2025-03-04 18:25:26 -1000

    add overloading.js

---
+++ overloading.js
@@ -0,0 +1,9 @@
+attendance()
+function attendance() {
+    console.log("Three sessions were attended today; called function")
+}
+var attendance = function(x) {
+    console.log(x + " sessions were attended today; called function expression")
+}
+attendance(4) // passes 4, outputs [4 sessions were attended today; called function expression]
+attendance() // passes undefined, outputs [undefined sessions were attended today; called function expression]
